f5324a913d3c3be98371c95b1acd3aec19d17d43
Puja Das <102240706+PujaDas@users.noreply.github.com>
2024-06-11 16:55:42 +0530

    Fixed Favorite songs related issues

--- src/pages/musicPlayer/MusicPlayer.js
+++ src/pages/musicPlayer/MusicPlayer.js
@@ -6,41 +6,41 @@ import { FaSearch, FaArrowLeft, FaHeart, FaMusic } from 'react-icons/fa';
 
 const MusicPlayer = () => {
   const initialPlaylist = [
-    { name: 'Love Me Like You Do', artist: 'Ellie Goulding', url: './audio/Ellie Goulding - Love Me Like You Do.mp3' },
-    { name: 'Without Me', artist: 'Halsey', url: './audio/Halsey - Without Me.mp3' },
-    { name: 'Changes', artist: 'XXXTENTACION', url: './audio/XXXTENTACION - Changes.mp3' },
-    { name: 'Dusk Till Dawn', artist: 'ZAYN & Sia', url: './audio/ZAYN & Sia - Dusk Till Dawn.mp3' },
-    { name: 'Nothing\'s Gonna Change My Love For You (Male Version)', artist: 'Westlife', url: './audio/Westlife - Nothing\'s Gonna Change My Love For You (Male Version).mp3' },
-    { name: 'Lover', artist: 'Taylor Swift', url: './audio/Taylor Swift - Lover.mp3' },
-    { name: 'I\'ll Be Waiting For You', artist: 'Gavy NJ', url: './audio/Gavy NJ - I\'ll Be Waiting For You.mp3' },
-    { name: 'You Are The Reason', artist: 'Calum Scott', url: './audio/Calum Scott - You Are The Reason.mp3' },
-    { name: 'Love The Way You Lie (Guitar Version)', artist: 'Jon Dretto', url: './audio/Jon Dretto - Love The Way You Lie (Guitar Version).mp3' },
-    { name: 'Nothing\'s Gonna Change My Love For You (Female Version)', artist: 'Shania Yan', url: './audio/Shania Yan - Nothing\'s Gonna Change My Love For You (Female Version).mp3' },
-    { name: 'Who Says', artist: 'Selena Gomez', url: './audio/Selena Gomez - Who Says.mp3' },
-    { name: 'Hold On', artist: 'Chord Overstreet', url: './audio/Chord Overstreet - Hold On.mp3' },
-    { name: 'All of Me', artist: 'John Legend ', url: './audio/John Legend - All of Me.mp3' },
-    { name: 'Baby', artist: 'Justin Bieber', url: './audio/Justin Bieber - Baby.mp3' },
-    { name: 'Best of Me', artist: 'Neffex', url: './audio/Neffex - Best of Me.mp3' },
-    { name: 'Dandelions', artist: 'Ruth B.', url: './audio/Ruth B. - Dandelions.mp3' },
-    { name: 'Lovely', artist: 'Billie Eilish', url: './audio/Billie Eilish - Lovely.mp3' },
-    { name: 'Perfect', artist: 'Ed Sheeran', url: './audio/Ed Sheeran - Perfect.mp3' },
-    { name: 'Photograph', artist: 'Ed Sheeran', url: './audio/Ed Sheeran - Photograph.mp3' },
-    { name: 'Unstoppable', artist: 'Sia', url: './audio/Sia - Unstoppable.mp3' },
-    { name: 'Thinking out Loud', artist: 'Ed Sheeran', url: './audio/Ed Sheeran - Thinking out Loud.mp3' },
-    { name: 'All I Want', artist: 'Alexandra Porat', url: './audio/All I Want - Kodaline cover by Alexandra Porat.mp3' },
-    { name: 'Not Your Barbie Girl', artist: 'Ava Max', url: './audio/Ava Max - Not Your Barbie Girl.mp3' },
-    { name: 'Love The Way You Lie 1, 2 & 3', artist: 'Eminem, Rihanna & Skylar Grey', url: './audio/Eminem, Rihanna & Skylar Grey - Love The Way You Lie 1, 2 & 3.mp3' },
-    { name: 'Happier x Perfect (Mashup)', artist: 'Ed Sheeran, Olivia Rodrigo', url: './audio/Happier x Perfect _ Ed Sheeran, Olivia Rodrigo (Mashup).mp3' },
-    { name: 'A Thousand Years', artist: 'Christina Perri', url: './audio/Christina Perri - A Thousand Years.mp3' },
-    { name: 'I Belong to You', artist: 'Jacob Lee', url: './audio/I Belong to You - Jacob Lee.mp3' },
-    { name: 'I Wanna Grow Old With You', artist: 'Westlife', url: './audio/I Wanna Grow Old With You - Westlife.mp3' },
-    { name: 'Love Story', artist: 'Taylor Swift', url: './audio/Love Story - Taylor Swift.mp3' },
-    { name: 'Happier', artist: 'Olivia Rodrigo', url: './audio/Olivia Rodrigo - Happier.mp3' },
-    { name: 'Memories', artist: 'Maroon 5', url: './audio/Maroon 5 - Memories.mp3' },
-    { name: 'See You Again', artist: 'Charlie Puth _ Christina Perri, Ellie Goulding', url: './audio/See You Again - Charlie Puth _ Christina Perri, Ellie Goulding (Mix).mp3' },
-    { name: 'Until I Found You', artist: 'Stephen Sanchez', url: './audio/Stephen Sanchez - Until I Found You.mp3' },
-    { name: 'My Heart Will Go On', artist: 'Celine Dion', url: './audio/Celine Dion - My Heart Will Go On.mp3' },
-    { name: 'Who Am I to Stand in Your Way', artist: 'Chester See', url: './audio/Chester See - Who Am I to Stand in Your Way.mp3' },
+    { id: 1, name: 'Love Me Like You Do', artist: 'Ellie Goulding', url: './audio/Ellie Goulding - Love Me Like You Do.mp3' },
+    { id: 2, name: 'Without Me', artist: 'Halsey', url: './audio/Halsey - Without Me.mp3' },
+    { id: 3, name: 'Changes', artist: 'XXXTENTACION', url: './audio/XXXTENTACION - Changes.mp3' },
+    { id: 4, name: 'Dusk Till Dawn', artist: 'ZAYN & Sia', url: './audio/ZAYN & Sia - Dusk Till Dawn.mp3' },
+    { id: 5, name: 'Nothing\'s Gonna Change My Love For You (Male Version)', artist: 'Westlife', url: './audio/Westlife - Nothing\'s Gonna Change My Love For You (Male Version).mp3' },
+    { id: 6, name: 'Lover', artist: 'Taylor Swift', url: './audio/Taylor Swift - Lover.mp3' },
+    { id: 7, name: 'I\'ll Be Waiting For You', artist: 'Gavy NJ', url: './audio/Gavy NJ - I\'ll Be Waiting For You.mp3' },
+    { id: 8, name: 'You Are The Reason', artist: 'Calum Scott', url: './audio/Calum Scott - You Are The Reason.mp3' },
+    { id: 9, name: 'Love The Way You Lie (Guitar Version)', artist: 'Jon Dretto', url: './audio/Jon Dretto - Love The Way You Lie (Guitar Version).mp3' },
+    { id: 10, name: 'Nothing\'s Gonna Change My Love For You (Female Version)', artist: 'Shania Yan', url: './audio/Shania Yan - Nothing\'s Gonna Change My Love For You (Female Version).mp3' },
+    { id: 11, name: 'Who Says', artist: 'Selena Gomez', url: './audio/Selena Gomez - Who Says.mp3' },
+    { id: 12, name: 'Hold On', artist: 'Chord Overstreet', url: './audio/Chord Overstreet - Hold On.mp3' },
+    { id: 13, name: 'All of Me', artist: 'John Legend ', url: './audio/John Legend - All of Me.mp3' },
+    { id: 14, name: 'Baby', artist: 'Justin Bieber', url: './audio/Justin Bieber - Baby.mp3' },
+    { id: 15, name: 'Best of Me', artist: 'Neffex', url: './audio/Neffex - Best of Me.mp3' },
+    { id: 16, name: 'Dandelions', artist: 'Ruth B.', url: './audio/Ruth B. - Dandelions.mp3' },
+    { id: 17, name: 'Lovely', artist: 'Billie Eilish', url: './audio/Billie Eilish - Lovely.mp3' },
+    { id: 18, name: 'Perfect', artist: 'Ed Sheeran', url: './audio/Ed Sheeran - Perfect.mp3' },
+    { id: 19, name: 'Photograph', artist: 'Ed Sheeran', url: './audio/Ed Sheeran - Photograph.mp3' },
+    { id: 20, name: 'Unstoppable', artist: 'Sia', url: './audio/Sia - Unstoppable.mp3' },
+    { id: 21, name: 'Thinking out Loud', artist: 'Ed Sheeran', url: './audio/Ed Sheeran - Thinking out Loud.mp3' },
+    { id: 22, name: 'All I Want', artist: 'Alexandra Porat', url: './audio/All I Want - Kodaline cover by Alexandra Porat.mp3' },
+    { id: 23, name: 'Not Your Barbie Girl', artist: 'Ava Max', url: './audio/Ava Max - Not Your Barbie Girl.mp3' },
+    { id: 24, name: 'Love The Way You Lie 1, 2 & 3', artist: 'Eminem, Rihanna & Skylar Grey', url: './audio/Eminem, Rihanna & Skylar Grey - Love The Way You Lie 1, 2 & 3.mp3' },
+    { id: 25, name: 'Happier x Perfect (Mashup)', artist: 'Ed Sheeran, Olivia Rodrigo', url: './audio/Happier x Perfect _ Ed Sheeran, Olivia Rodrigo (Mashup).mp3' },
+    { id: 26, name: 'A Thousand Years', artist: 'Christina Perri', url: './audio/Christina Perri - A Thousand Years.mp3' },
+    { id: 27, name: 'I Belong to You', artist: 'Jacob Lee', url: './audio/I Belong to You - Jacob Lee.mp3' },
+    { id: 28, name: 'I Wanna Grow Old With You', artist: 'Westlife', url: './audio/I Wanna Grow Old With You - Westlife.mp3' },
+    { id: 29, name: 'Love Story', artist: 'Taylor Swift', url: './audio/Love Story - Taylor Swift.mp3' },
+    { id: 30, name: 'Happier', artist: 'Olivia Rodrigo', url: './audio/Olivia Rodrigo - Happier.mp3' },
+    { id: 31, name: 'Memories', artist: 'Maroon 5', url: './audio/Maroon 5 - Memories.mp3' },
+    { id: 32, name: 'See You Again', artist: 'Charlie Puth _ Christina Perri, Ellie Goulding', url: './audio/See You Again - Charlie Puth _ Christina Perri, Ellie Goulding (Mix).mp3' },
+    { id: 33, name: 'Until I Found You', artist: 'Stephen Sanchez', url: './audio/Stephen Sanchez - Until I Found You.mp3' },
+    { id: 34, name: 'My Heart Will Go On', artist: 'Celine Dion', url: './audio/Celine Dion - My Heart Will Go On.mp3' },
+    { id: 35, name: 'Who Am I to Stand in Your Way', artist: 'Chester See', url: './audio/Chester See - Who Am I to Stand in Your Way.mp3' },
   ];
 
   const [playlist, setPlaylist] = useState(() => initialPlaylist.sort((a, b) => a.name.localeCompare(b.name)));
@@ -59,19 +59,20 @@ const MusicPlayer = () => {
   }, [currentSongIndex, isPlaying]);
 
   const handleClickNext = () => {
-    setCurrentSongIndex((prevIndex) => (prevIndex + 1) % playlist.length);
+    setCurrentSongIndex((prevIndex) => (prevIndex + 1) % displayedPlaylist.length);
     setIsPlaying(true);
   };
 
   const handleClickPrevious = () => {
     setCurrentSongIndex((prevIndex) =>
-      prevIndex === 0 ? playlist.length - 1 : prevIndex - 1
+      prevIndex === 0 ? displayedPlaylist.length - 1 : prevIndex - 1
     );
     setIsPlaying(true);
   };
 
   const handleSongClick = (index) => {
-    setCurrentSongIndex(index);
+    const actualIndex = playlist.findIndex(song => song.id === displayedPlaylist[index].id);
+    setCurrentSongIndex(actualIndex);
     setIsPlaying(true);
     if (audioRef.current) {
       audioRef.current.audio.current.play().catch(error => {
@@ -140,7 +141,7 @@ const MusicPlayer = () => {
           className={`tab ${showFavorites ? 'active' : ''}`}
           onClick={() => setShowFavorites(true)}
         >
-          <FaHeart />
+          Favorite Songs
         </span>
       </div>
       {displayedPlaylist.length === 0 ? (
@@ -153,7 +154,7 @@ const MusicPlayer = () => {
           {displayedPlaylist.map((song, index) => (
             <li
               key={song.id}
-              className={`playlist-item ${currentSongIndex === index ? 'active' : ''}`}
+              className={`playlist-item ${playlist[currentSongIndex]?.id === song.id ? 'active' : ''}`}
               onClick={() => handleSongClick(index)}
             >
               <div className='song-info'>
@@ -187,4 +188,3 @@ const MusicPlayer = () => {
 };
 
 export default MusicPlayer;
-
